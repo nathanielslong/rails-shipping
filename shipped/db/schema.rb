084288e117341bec84e6271688bd3fd0a77e7a42
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-02-27 19:48:19 -0500

    got ports migration to work

--- shipped/db/schema.rb
+++ shipped/db/schema.rb
@@ -10,9 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170228003740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "boats", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "location"
+    t.integer  "total_containers"
+    t.integer  "available_containers"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.index ["user_id"], name: "index_boats_on_user_id", using: :btree
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.text     "description"
+    t.string   "origin"
+    t.string   "destination"
+    t.decimal  "cost"
+    t.integer  "needed_containers"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.index ["user_id"], name: "index_jobs_on_user_id", using: :btree
+  end
+
+  create_table "ports", force: :cascade do |t|
+    t.string   "city"
+    t.string   "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "routes", force: :cascade do |t|
+    t.integer  "boat_id"
+    t.integer  "job_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["boat_id"], name: "index_routes_on_boat_id", using: :btree
+    t.index ["job_id"], name: "index_routes_on_job_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "company"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "boats", "users"
+  add_foreign_key "jobs", "users"
+  add_foreign_key "routes", "boats"
+  add_foreign_key "routes", "jobs"
 end
