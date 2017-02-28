2edaee1ffda5c27c485150f41725ded2b36993ac
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-02-27 21:06:33 -0500

    got ports added to the database

--- shipped/db/seeds.rb
+++ shipped/db/seeds.rb
@@ -1,3 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
 port_cities = ["Shanghai",
 "Singapore",
 "Guangzhou",
@@ -201,3 +209,7 @@ port_countries = ["China",
 "France"]
 
 ports = Hash[port_cities.zip(port_countries)]
+
+ports.each do |key, val|
+  Port.create(city: key, country: val)
+end
