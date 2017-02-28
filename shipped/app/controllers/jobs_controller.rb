ef32a1ca5f21aeb0bc0ca47ab90f7eb323def88e
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-02-28 16:01:33 -0500

    adding views to forms and controller, as well as jobs show

--- shipped/app/controllers/jobs_controller.rb
+++ shipped/app/controllers/jobs_controller.rb
@@ -1,15 +1,16 @@
 class JobsController < ApplicationController
+  before_action :find_job, only: [:edit, :show]
+
   def index
   end
 
   def new
-    @jobs = Job.new()
+    @job = Job.new()
 
     @ports = Port.all.collect{ |port| [port.location, port.id]  }
   end
 
   def edit
-    @jobs = Job.find(params[:id])
   end
 
   def show
@@ -17,9 +18,12 @@ class JobsController < ApplicationController
 
   private
 
-  def jobs_params
-    params.require(:job).permit(:user_id, :name, :description, :origin, :destination, :cost, :needed_containers)
+  def job_params
+    params.require(:job).permit(:id, :user_id, :name, :description, :origin, :destination, :cost, :needed_containers, :updated_at)
   end
 
+  def find_job
+    @job = Job.find(params[:id])
+  end
 
 end
