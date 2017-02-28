ebc358e24951a472bc35065dcf13de82a82532f9
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-02-28 16:17:36 -0500

    property indented the boat model

--- shipped/app/models/boat.rb
+++ shipped/app/models/boat.rb
@@ -5,15 +5,15 @@ class Boat < ApplicationRecord
 
   validates_uniqueness_of :name
   has_attached_file :image,
-					  styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" },
-					  default_url: "/images/:style/missing.png",
-					  :storage => :s3,
-					  :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
-					  s3_region: ENV["AWS_REGION"]
-	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-	
-	def s3_credentials
-		{:bucket => "railsblogbucket", :access_key_id => ENV["AWS_ACCESS_KEY_ID"], :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]}
-	end
+    styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" },
+    default_url: "/images/:style/missing.png",
+    :storage => :s3,
+    :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
+    s3_region: ENV["AWS_REGION"]
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  def s3_credentials
+    {:bucket => "railsblogbucket", :access_key_id => ENV["AWS_ACCESS_KEY_ID"], :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]}
+  end
 
 end
