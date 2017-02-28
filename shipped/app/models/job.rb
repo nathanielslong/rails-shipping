76493a0db2083e4faacbbb81d3b6264e9e09aede
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-02-27 19:21:44 -0500

    postgresql database connected

--- shipped/app/models/job.rb
+++ shipped/app/models/job.rb
@@ -1,4 +1,8 @@
 class Job < ApplicationRecord
   has_many :routes
+
   belongs_to :user
+
+  validates :cost, numericality: { greater_than: 1000, too_cheap: "The job must cost more than $1000!" }
+  validates_uniqueness_of :name
 end
