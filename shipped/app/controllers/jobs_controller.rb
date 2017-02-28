806a93c8509c4e3e51eda909466865f1ab7f0610
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-02-28 17:43:32 -0500

    added more controller functions and views details

--- shipped/app/controllers/jobs_controller.rb
+++ shipped/app/controllers/jobs_controller.rb
@@ -1,9 +1,9 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :find_job, only: [:edit, :show]
+  before_action :find_job, only: [:edit, :show, :update]
 
   def index
-    @jobs = Job.all
+    @jobs = Job.all.order("id DESC")
   end
 
   def new
@@ -12,7 +12,31 @@ class JobsController < ApplicationController
     @ports = Port.all.collect{ |port| [port.location, port.id]  }
   end
 
+  def create
+    @job = User.find(current_user).jobs.new
+    @job.update_attributes(job_params)
+
+    if @job.save
+      flash[:notice] = "Successfully created job!"
+      redirect_to job_path(@job)
+    else
+      flash[:alert] = "Error creating new job."
+      render :new
+    end
+
+  end
+
   def edit
+  end
+
+  def update
+    if @job.update_attributes(job_params)
+      flash[:notice] = "Successfully created job!"
+      redirect_to job_path(@job)
+    else
+      flash[:alert] = "Error creating new job."
+      render :edit
+    end
   end
 
   def show
