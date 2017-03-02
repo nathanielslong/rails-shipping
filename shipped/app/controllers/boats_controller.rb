4647cd939301ad0c7cb14d7e9fb1b215f3d801cd
Jack Linton <jacklinton@mac.com>
2017-03-02 13:10:05 -0500

    Added new boat functionality with a new form partial and path to correspond. Also edited the boat editing functionality to have a pull down menu for locations, and now all the attributes of the oats can be edited, not just the image. The redirects all work. However, there is on problem of the boat location being stored as an integer rather than a string containing the name of the location.

--- shipped/app/controllers/boats_controller.rb
+++ shipped/app/controllers/boats_controller.rb
@@ -1,24 +1,52 @@
 class BoatsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_boat, only: [:update, :show, :edit]
 
   def index
     @boats = Boat.all
   end
 
-  def update
-    @boat.image = boat_params[:image]
-    @boat.save
+  def edit
+    @ports = Port.all.collect{ |port| [port.location, port.id]  }
+  end
 
-    redirect_to action: show, id: @boat.id 
+
+  def update
+    if @boat.update_attributes(boat_params)
+      flash[:notice] = "Successfully created new boat!"
+      redirect_to boat_path(@boat)
+    else
+      flash[:alert] = "Error creating new boat."
+      render :edit
+    end
+
   end
 
   def show
 
-    
+    @ports = Port.all.collect{ |port| [port.location, port.id]  }
   end
 
   def new
+    @boat = Boat.new()
+
+    @ports = Port.all.collect{ |port| [port.location, port.id]  }
   end
+
+  def create
+    @boat = Boat.last
+    @boat.update_attributes(boat_params)
+
+    if @boat.save
+      flash[:notice] = "Successfully created boat!"
+      redirect_to boat_path(@boat)
+    else
+      flash[:alert] = "Error creating new boat."
+      render :new
+    end
+
+  end
+
 
   def _form
   end
