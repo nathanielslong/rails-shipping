b74789f2770fdb6bccb963e32ac80e9603c3a445
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-02 14:18:22 -0500

    boat id, not id. wow I stared at that too long

--- shipped/app/models/user.rb
+++ shipped/app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
     not_located = self.boats.where("location != '#{origin}'")
 
     #need to test
-    available_boats = (self.boats.all - Boat.find(assigned_boats.map(&:id)) - Boat.find(not_enough_space.map(&:id)) - Boat.find(not_located.map(&:id))).map{ |e| [e.name, e.id] }
+    available_boats = (self.boats.all - Boat.find(assigned_boats.map(&:boat_id)) - Boat.find(not_enough_space.map(&:id)) - Boat.find(not_located.map(&:id))).map{ |e| [e.name, e.id] }
   end
 
   def full_name
