5af0441af5e95fb40afcd89545dfe7216dabd354
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-02 10:25:04 -0500

    added more validations to cost (proper cost format)

--- shipped/app/models/job.rb
+++ shipped/app/models/job.rb
@@ -4,8 +4,12 @@ class Job < ApplicationRecord
   belongs_to :user
 
   validates :cost, numericality: { greater_than: 1000, too_cheap: "The job must cost more than $1000!" }
+  validates_format_of :cost, :with => /\A\d+(?:\.\d{0,2})?\z/
+
   validates :needed_containers, numericality: { only_integer: true, not_integer: "Containers must be integers." }
+
   validates :description, length: { minimum: 50, too_short: "Your description must be at least 50 characters." }
+
   validates_uniqueness_of :name
 
 end
--- shipped/app/models/user.rb
+++ shipped/app/models/user.rb
@@ -8,13 +8,15 @@ class User < ApplicationRecord
   has_many :jobs
 
   def available_boats(needed_containers, origin, job_id)
-    #test in console
     #search aray for particular boat id job id combo(make)
     assigned_boats = Route.where(job_id: job_id)
+
     #checks container number
     enough_space = self.boats.where("available_containers >= #{needed_containers}")
+
     #checks location number
     located = self.boats.where(location: origin)
+
     #need to test
     available_boats = (self.boats.all - Boat.find(assigned_boats.map(&:boat_id)) - Boat.find(enough_space.map(&:boat_id)) - Boat.find(located.map(&:boat_id))).map{ |e| [e.name, e.id] }
   end
