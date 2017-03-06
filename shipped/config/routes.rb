051bce365e884a1d9c294bd269b1c0ac798a5c66
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-06 10:00:01 -0500

    edit paage was redirecting to an unexpected view, deleted and edited routes

--- shipped/config/routes.rb
+++ shipped/config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   #make routing for form
-  # get 'jobs/:id/route' => 'jobs#route', as: "routes_path"
-
-  get 'registrations/update'
 
   devise_for :users, :controllers => {:registrations => "registrations"}
 
   root to: 'home#index'
-
-  # get '/users/sign-up', to: "registrations#new"
 
   resources :users, :only => [:show]
 
